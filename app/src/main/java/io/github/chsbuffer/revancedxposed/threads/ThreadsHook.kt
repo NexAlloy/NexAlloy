da1436af0960b3d76f03e35c9d2e9853a0236f1a
gnadgnaoh <117189041+gnadgnaoh@users.noreply.github.com>
2026-03-10 21:37:05 +0700

    Create ThreadsHook.kt

---
+++ app/src/main/java/io/github/chsbuffer/revancedxposed/threads/ThreadsHook.kt
@@ -0,0 +1,6 @@
+package io.github.chsbuffer.revancedxposed.threads
+
+import io.github.chsbuffer.revancedxposed.threads.ads.HideAds
+import io.github.chsbuffer.revancedxposed.threads.network.BlockNetwork
+
+val ThreadsPatches = arrayOf(HideAds, BlockNetwork)
