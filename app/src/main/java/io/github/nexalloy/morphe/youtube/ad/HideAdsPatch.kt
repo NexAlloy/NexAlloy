9a534ab2e7d4eeabb07158df610efac56e766da3
Yiğit <40565628+tryigit@users.noreply.github.com>
2026-09-05 03:25:10 +0300

    Restore stable YouTube ad hook baseline

--- app/src/main/java/io/github/nexalloy/morphe/youtube/ad/HideAdsPatch.kt
+++ app/src/main/java/io/github/nexalloy/morphe/youtube/ad/HideAdsPatch.kt
@@ -10,22 +10,12 @@ import io.github.nexalloy.morphe.shared.ad.HideFullscreenAds
 import io.github.nexalloy.morphe.shared.misc.litho.filter.addLithoFilter
 import io.github.nexalloy.morphe.shared.misc.settings.preference.SwitchPreference
 import io.github.nexalloy.morphe.youtube.layout.hide.general.HideHorizontalShelves
-import io.github.nexalloy.morphe.youtube.misc.contexthook.ClientContextEndpoint.BROWSE
-import io.github.nexalloy.morphe.youtube.misc.contexthook.ClientContextEndpoint.GET_WATCH
-import io.github.nexalloy.morphe.youtube.misc.contexthook.ClientContextEndpoint.GUIDE
-import io.github.nexalloy.morphe.youtube.misc.contexthook.ClientContextEndpoint.NEXT
-import io.github.nexalloy.morphe.youtube.misc.contexthook.ClientContextEndpoint.PLAYER
-import io.github.nexalloy.morphe.youtube.misc.contexthook.ClientContextEndpoint.REEL
-import io.github.nexalloy.morphe.youtube.misc.contexthook.ClientContextEndpoint.SEARCH
-import io.github.nexalloy.morphe.youtube.misc.contexthook.ClientContextHook
-import io.github.nexalloy.morphe.youtube.misc.contexthook.addOSNameHook
 import io.github.nexalloy.morphe.youtube.misc.engagement.EngagementPanelHook
 import io.github.nexalloy.morphe.youtube.misc.engagement.addEngagementPanelIdHook
 import io.github.nexalloy.morphe.youtube.misc.litho.filter.LithoFilter
 import io.github.nexalloy.morphe.youtube.misc.playservice.VersionCheck
 import io.github.nexalloy.morphe.youtube.misc.settings.PreferenceScreen
 import io.github.nexalloy.patch
-import io.github.nexalloy.scopedHook
 
 val HideAds = patch(
     name = "Hide ads",
@@ -35,14 +25,14 @@ val HideAds = patch(
         LithoFilter,
         EngagementPanelHook,
         HideHorizontalShelves,
+
         HideFullscreenAds(PreferenceScreen.ADS),
         VersionCheck,
-        ClientContextHook,
     )
 
     PreferenceScreen.ADS.addPreferences(
         SwitchPreference("morphe_hide_creator_store_shelf"),
-        SwitchPreference("morphe_hide_end_screen_store_banner"),
+//        SwitchPreference("morphe_hide_end_screen_store_banner"),
         SwitchPreference("morphe_hide_general_ads"),
         SwitchPreference("morphe_hide_merchandise_banners"),
         SwitchPreference("morphe_hide_paid_promotion_labels"),
@@ -56,37 +46,20 @@ val HideAds = patch(
     addLithoFilter(AdsFilter())
     addEngagementPanelIdHook(AdsFilter::hidePlayerPopupAds)
 
-    listOf(BROWSE, SEARCH, NEXT).forEach { endpoint ->
-        addOSNameHook(endpoint, AdsFilter::hideAds)
-    }
-    listOf(GET_WATCH, PLAYER, REEL).forEach { endpoint ->
-        addOSNameHook(endpoint, AdsFilter::hideVideoAds)
-    }
-    addOSNameHook(GUIDE, AdsFilter::overrideGuideOSName)
-
+    // Hide video ads
     setOf(
         LoadVideoAdsFingerprint,
         PlayerBytesAdLayoutFingerprint,
     ).forEach { fingerprint ->
         fingerprint.hookMethod {
             before {
-                if (AdsFilter.hideVideoAds()) it.result = null
+                if (AdsFilter.hideVideoAds())
+                    it.result = null
             }
         }
     }
 
-    FullScreenEngagementAdContainerFingerprint.hookMethod(
-        scopedHook(::fullScreenEngagementAdAddMethod.member) {
-            before { param ->
-                if (innerDepth != 0) return@before
-                @Suppress("UNCHECKED_CAST")
-                val elements = param.thisObject as MutableList<Any?>
-                AdsFilter.hideEndScreenStoreBanner(elements, param.args[0])
-                param.result = true
-            }
-        }
-    )
-
+    // Hide get premium
     GetPremiumViewFingerprint.hookMethod {
         after {
             if (AdsFilter.hideGetPremiumView()) {
@@ -96,6 +69,8 @@ val HideAds = patch(
         }
     }
 
+    // Hide player overlay view. This can be hidden with a regular litho filter
+    // but an empty space remains.
     PlayerOverlayTimelyShelfFingerprint.hookMethod {
         val playerOverlayEventClass = ::PlayerOverlayEventType.clazz
         val playerOverlayIdField = ::PlayerOverlayIdField.field
@@ -103,12 +78,16 @@ val HideAds = patch(
             val obj = it.args[0]
             if (playerOverlayEventClass.isInstance(obj)) {
                 val id = playerOverlayIdField.get(obj) as String
-                if (!AdsFilter.allowAds(id == "player_overlay_timely_shelf")) it.result = null
+
+                if (!AdsFilter.allowAds(id == "player_overlay_timely_shelf"))
+                    it.result = null
             }
         }
     }
 
+    // Hide ad views
     val adAttributionId = ResourceUtils.getIdIdentifier("ad_attribution")
+
     XposedHelpers.findAndHookMethod(
         View::class.java.name,
         lpparam.classLoader,
@@ -123,14 +102,4 @@ val HideAds = patch(
                 }
             }
         })
-
-    val miniplayerSubtitleId = ResourceUtils.getIdIdentifier("modern_miniplayer_subtitle_text")
-    MiniplayerPaidPromotionLabelFingerprint.hookMethod(
-        scopedHook(::miniplayerPaidPromotionViewMethod.member) {
-            after { param ->
-                if (param.args[0] != miniplayerSubtitleId) return@after
-                (param.result as? View)?.let(AdsFilter::hideMiniplayerPaidPromotionLabelView)
-            }
-        }
-    )
 }
